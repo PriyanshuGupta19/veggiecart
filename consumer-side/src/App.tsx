c612ec61e6225f4d471e44b25437959d86d85e27
PriyanshuGupta19 <164480200+PriyanshuGupta19@users.noreply.github.com>
2025-02-28 10:37:02 +0530

    located Dashboard.tsx in components of consumer-side

--- consumer-side/src/App.tsx
+++ consumer-side/src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "../auth/LandingPage"; // Import from auth
 import SignUp from "../auth/SignUp";
 import Login from "../auth/Login";
-import Dashboard from "../auth/Dashboard";
+import Dashboard from "./components/Dashboard";
 const App = () => {
     return (
         <Routes>
---
+++ consumer-side/src/components/Dashboard.tsx
@@ -0,0 +1,18 @@
+import Navbar from "./navbar";
+import FirstBody from "./firstbody";
+import SecondBody from "./secondbody";
+import Products from "./products";
+import Footer from "./footer";
+const Dashboard = () => {
+    return (
+      <>
+   <Navbar />
+   <FirstBody />
+   <SecondBody />
+   <Products />
+   <Footer />
+      </>
+    )
+  };
+  
+  export default Dashboard;
